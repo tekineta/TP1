68459881f0cc69ec10b3e6d88064efe2877c3d0b
tekineta <111514255+tekineta@users.noreply.github.com>
2023-03-15 09:05:50 -0300

    Add files via upload

---
+++ scripts-dom.js
@@ -0,0 +1,48 @@
+/*var elTituloDeLaPagina = document.getElementById("titulo");
+var losTitulos = document.getElementsByTagName("h1");
+var titulosConClaseNegro = document.getElementsByClassName("texto-negro");
+var otroTituloEnH2 = document.createElement("h2");
+var unDiv = document.createElement("div");
+var unaImagen = document.createElement("img");
+
+console.log(elTituloDeLaPagina);
+console.log(losTitulos);
+console.log(titulosConClaseNegro);
+
+
+elTituloDeLaPagina.innerHTML = "Hola 5B!!";
+elTituloDeLaPagina.style.color = "green";
+
+otroTituloEnH2.innerHTML = "Esto es otro titulo mas chiquito";
+unaImagen.src = "https://media.tycsports.com/files/2023/02/10/532928/lionel-messi_862x485.webp";
+
+document.body.appendChild(otroTituloEnH2);
+unDiv.appendChild(unaImagen);
+document.body.appendChild(unDiv);
+
+var inputUsuario = document.getElementById("usuario");
+
+inputUsuario.onkeyup = validarUsuario;
+
+var button = document.getElementById("boton");
+
+button.onclick = function() {
+    alert(inputUsuario.value);
+}
+
+
+
+
+function validarUsuario(event) {
+    console.log(event.target.value);
+
+    if (event.target.value.length > 8) {
+        event.target.style.color = "red";
+    } else {
+        event.target.style.color = "green";
+    }
+}*/
+
+var NotaMatematica = document.getElementById(h1);
+var NotaLengua = document.getElementById(h1);
+var NotaEfsi = document.getElementById(h1);
---
+++ scripts.js
@@ -0,0 +1,49 @@
+/*var nombre = "Facundito";
+var edad = 17;
+var esRubio = false;
+
+for (var i = 0; i < 2; i++) {
+    if (nombre == "Facu") {
+        alert("Hola Facu");
+    } else {
+        alert("Hola otra persona");
+    }
+}
+
+
+console.log("Hola Gente!!!");
+console.info("Hola Info!!!");
+console.warn("Hola Warn!!!");
+console.error("Hola Error!!!");
+
+
+function esMayorDeEdad(edad) {
+    return edad >= 18;
+}
+
+
+if (!esMayorDeEdad(edad)) {
+    console.log("Es Menor");
+} else {
+    console.log("Es Mayor");
+}
+
+var edadDeChino = "17";
+
+function sumar(num1, num2) {
+    return num1 + num2;
+}
+
+var lasEdadesSumadas = sumar(edad, edadDeChino);
+
+console.log(lasEdadesSumadas);
+
+console.log(esRubio + edad);
+console.log(nombre + edad);
+
+function tiene17(edad) {
+    return edad === 17;
+}
+
+
+console.log(tiene17(edadDeChino));*/
